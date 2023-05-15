c729ab7a96e83d12497eb91bc057b0f820c54df7
William Alves <willoandrade29@gmail.com>
2023-05-15 19:22:27 -0300

    feat: add migrations
    Migrations will help us to keep track of any database-related changes.

---
+++ API/Data/Migrations/20230515221104_CreateDatabase.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Musics",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Duration = table.Column<double>(type: "REAL", nullable: false),
+                    Artist = table.Column<string>(type: "TEXT", nullable: false),
+                    Album = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Musics", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Musics");
+        }
+    }
+}
