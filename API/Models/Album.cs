a8ac8c82c16a56b1880555c32ae9daefb305f533
William Alves <willoandrade29@gmail.com>
2023-05-18 21:23:23 -0300

    refactor: add datatype annotation
    By doing this, the JSON format for the ReleaseDate is easier to write.

--- API/Models/Album.cs
+++ API/Models/Album.cs
@@ -5,8 +5,8 @@ namespace API.Models
     public class Album
     {
         public int Id { get; set; }
-        public DateTime ReleaseDate { get; set; }
         public int TotalMusics { get; set; }
+        [DataType(DataType.Date)]public DateTime ReleaseDate { get; set; }
         [Required] public string Name { get; set; }
         [Required] public Artist Artist { get; set; }
     }
